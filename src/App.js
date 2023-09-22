97611c74fcf706dc65853001b5434996c9bb1d88
Arun-Karthik0 <arunkarthik07selvam@gmail.com>
2023-09-22 12:59:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [total,setTotal]= useState('');
+
+  const handleclick = (e) => {
+   setTotal(total.concat(e.target.name))
+  }
+  const clear = () => {
+  setTotal("");
+  }
+  function handledelete(){
+    setTotal(total.slice(0,-1));
+  }
+  const calculate = () => {
+    try {
+      setTotal(eval(total).toString());
+    } catch {
+      setTotal("Error")
+    }
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>modern calculator</h1>
+      
+      <div className='container'>
+        <form>
+          <input type='text' value={total} />
+        </form>
+        <div className='keyboard'>
+          <button onClick={clear}id='clear' className='highlight'>AC</button>
+          <button onClick={handledelete}className='highlight'>DEL</button>
+          <button name='/' onClick={handleclick} className='highlight'>&divide;</button>
+          <button name='7' onClick={handleclick}>7</button>
+          <button name='8' onClick={handleclick}>8</button>
+          <button name='9' onClick={handleclick}>9</button>
+          <button name='*' onClick={handleclick}className='highlight'>&times;</button>
+          <button name='4' onClick={handleclick}>4</button>
+          <button name='5' onClick={handleclick}>5</button>
+          <button name='6' onClick={handleclick}>6</button>
+          <button name='-' onClick={handleclick}className='highlight'>-</button>
+          <button name='1' onClick={handleclick}>1</button>
+          <button name='2' onClick={handleclick}>2</button>
+          <button name='3' onClick={handleclick}>3</button>
+          <button name='+' onClick={handleclick}className='highlight'>+</button>
+          <button name='0' onClick={handleclick}>0</button>
+          <button name='.' onClick={handleclick}>.</button>
+          <button onClick={calculate} id='equal' className='highlight'>=</button>
+        </div>
+        </div>
+        </div>
   );
 }
 
